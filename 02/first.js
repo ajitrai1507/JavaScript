c190636f71abd3bf641bf36ecfee1ffe2adcf3ca
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-14 18:28:46 +0530

    primitive data type

---
+++ 02/first.js
@@ -0,0 +1,39 @@
+//Data types
+
+//Primitive Data Type///
+
+//1 Number
+
+let num = 10;
+console.log(num);
+
+let account_balance = 10;
+console.log(account_balance);
+//2 String
+let str = "Ajit";
+console.log(str);
+
+//3 Boolean
+
+let statement = true;
+console.log(statement);
+console.log(typeof statement);
+
+//4 Undefined
+
+let account;
+
+console.log(account);
+console.log(typeof account);
+
+//5 Null
+
+let bal = null;
+console.log(bal);
+console.log(typeof bal);
+
+//6 Bigint
+
+let a = 4544521341321216542354534324;
+console.log(a);
+console.log(typeof a);
