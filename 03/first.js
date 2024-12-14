330ac0f710c3d9ff72a3c459f7da1403ec996bbe
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-14 21:45:24 +0530

    Non-primitive data types

---
+++ 03/first.js
@@ -0,0 +1,28 @@
+//Non-Primitive Data types
+
+//Array
+
+let nums = [10,20,30];
+console.log(nums);
+console.log(typeof nums);
+
+//Object
+
+let student = {
+
+    name: "Ajit",
+    rollNo: 22,
+    location: "Delhi",
+    pin: 110043
+}
+
+console.log(student);
+console.log(typeof student);
+
+//Function
+
+let names = function ajit(){
+    console.log("Hello ajit rai");
+}
+
+console.log(typeof names)
