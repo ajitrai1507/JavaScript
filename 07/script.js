92016c2c60affe443a22734060c32c05f31c0304
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-17 14:36:50 +0530

    Math and Number

---
+++ 07/script.js
@@ -0,0 +1,17 @@
+//Number is JavaScript
+
+// let num = 231;
+// let num2 = new Number(231);
+
+// console.log(typeof num);
+// console.log(num2);
+// console.log(typeof num2);
+
+// let num = 236.1;
+// console.log(num.toFixed());
+
+// console.log(Math)
+
+// console.log(Math.floor(Math.random()*6)+1);
+
+console.log(Math.floor(Math.random()*10)+11);
