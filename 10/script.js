5e9e234d470f4799b890ff4d4316e61d7816b6b8
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-17 23:23:49 +0530

    Accessing elements

---
+++ 10/script.js
@@ -0,0 +1,8 @@
+//Accessing By Css Selectors
+
+const id = document.querySelector("#first");
+id.innerHTML = "Hello Ajit Rai";
+
+const h2 = document.querySelector(".header2");
+h2.style.backgroundColor = "Pink";
+h2.style.color = "white";
