bb45275a826c29377fdcf6bebaece0fd0d044ded
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-17 13:56:39 +0530

    string

---
+++ 06/string.js
@@ -0,0 +1,24 @@
+//String in JavaScript
+
+// let str = "Hello Ajit Rai";
+// console.log(str);
+
+// let str1 = 'Hello Coder Army';
+// console.log(str1);
+
+// let str2 = `Hello Coders`;
+// console.log(str2);
+
+// let price = 50;
+// console.log(`Price of tomato is ${price}`);
+
+let firstName = "Ajit";
+let lastName = " Rai";
+
+let fullName = firstName.concat(lastName);
+// console.log(fullName);
+// console.log(firstName[0]);
+// console.log(`"hello coder army"`);
+
+let heros = "Hello coder army";
+console.log(heros.indexOf("coder"));
