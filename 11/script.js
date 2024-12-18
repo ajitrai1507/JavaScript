e98ae326258eec540d37762df6806ce7bcfe6259
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-18 11:10:04 +0530

    Array

---
+++ 11/script.js
@@ -0,0 +1,44 @@
+//Array in JavaSceript
+//To check the length of the array 
+let arr = ["aj",5,true, "sdjkhvfh", "jkfjd","hero","heroine"];
+// console.log(arr.length);
+
+//To add the element in the array in the last
+
+// arr.push("Ajit Rai");
+// console.log(arr);
+// console.log(arr.length);
+
+//To remove the last element in the array
+
+// console.log(arr.pop());
+// console.log(arr)
+
+//To access the specific element in the array
+
+// console.log(arr[1]);
+
+// const newArr = arr;
+// // console.log(newArr==arr);
+// newArr.push("JK cement");
+// console.log(arr);
+// console.log(newArr)
+
+// arr.unshift("Khalifa");
+// arr.unshift("Mia");
+// arr.shift();
+// arr.shift();
+// arr.delete(5);
+// console.log(arr);
+// console.log(arr.indexOf("hero"));
+// console.log(arr.indexOf(true));
+
+// console.log(arr.includes("John"))
+
+// console.log(arr.slice(2,4));
+// console.log(arr.slice(2,6));
+
+// console.log(arr.splice(1,2))
+// console.log(arr)
+
+console.log(arr.toString());
