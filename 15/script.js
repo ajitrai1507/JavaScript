0eb6fc043202885f2e194bdf1ef6e35d7ebda3b5
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-21 13:35:26 +0530

    Object in JS

---
+++ 15/script.js
@@ -0,0 +1,73 @@
+//Object in JS
+
+// let stu = {
+//     0:20,
+//     1:50,
+//     name: "Ajit Rai",
+//     age: 22,
+//     location: "New Delhi",
+//     "Mobile No.": 75031,
+//     undefined : 30
+// }
+
+// console.log(stu);
+// console.log(stu.age);
+// console.log(stu["Mobile No."]);
+// console.log(stu['0']);
+// console.log(stu[1]);
+// console.log(stu.undefined);
+
+// let stu = {};
+
+// stu.firstName = "Ajit"
+// stu.lastName = "Rai"
+// stu.age = 22
+
+// console.log(stu);
+
+// class People{
+//     constructor(name,age,gender){
+//         this.name = name,
+//         this.age = age,
+//         this.gender = gender
+//     }
+// }
+
+// let per1 = new People("Rohit", 22, "Male");
+// let per2 = new People("Ajit Rai", 23, "Male");
+// console.log(typeof per2);
+// console.log(per2);
+// console.log(per1);
+
+
+let obj = {
+    name: "Ajit Rai",
+    age: 22,
+    account_bal: 420,
+    gender: "Male"
+}
+
+// console.log(obj);
+//Keys
+// const arr = Object.keys(obj);
+// console.log(arr)
+
+//Values
+
+// const arr = Object.values(obj);
+// console.log(arr)
+
+// keys values
+
+// const arr = Object.entries(obj);
+// console.log(arr)
+
+//assign object
+
+// const obj1 = {a:1, b:2};
+// const obj2 = {c:3, d:4}
+
+// const obj3 = Object.assign({},obj1, obj2);
+// console.log(obj3);
+// console.log(obj1)
+
