2cc669c5248a6178c79254fa159fb67d24ef4fef
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-22 23:45:11 +0530

    Function in JS

--- 17/script.js
+++ 17/script.js
@@ -1,1 +1,81 @@
 //Function In JS
+
+// function greet(){
+//     console.log("Hello coder army");
+// }
+
+// greet();
+
+// function add(){
+//     return 2+3;
+// }
+
+// const sum = console.log(add());
+
+// function sum(num1, num2){
+//     // console.log(num1+num2);
+//     return num1+num2
+// }
+
+// const total = sum(2,5);
+// console.log(total)
+
+// function mul(num1, num2){
+//     console.log(num1*num2);
+// }
+
+// mul(2,5);
+
+// const fun = function(){
+//     console.log("Hello ajit rai")
+//     console.log("Hello ajit rai")
+//     return "Money";
+// }
+
+// console.log(fun());
+//Arrow Function
+
+// const add = ()=>{
+//     console.log("Heyy Ajit Rai");
+// }
+
+// add()
+
+// const fun = ()=>{
+//     console.log("Hello coder");
+// }
+
+// fun();
+
+// const cube = (num)=> num*num*num;
+
+// console.log(cube(2));
+
+//Rest operator with function
+
+// function sum(...number){
+//     console.log(number);
+// }
+
+// sum(2,5,6);
+// sum(2,5,8,9,6,1);
+// sum(2,5);
+
+// const obj1 = {
+//     name: "Ajit",
+//     amount: 456,
+//     location: "Delhi"
+// }
+
+// function fun(obj1){
+//     console.log(obj1.name, obj1.amount);
+// }
+
+//***************************Object destructure********************************** */
+
+// function fun({name, location}){
+//     console.log(name, location)
+// }
+
+// fun(obj1)
+
---
+++ 17/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+function fun({name, location}){
+//     console.log(name, location)
+// }
