68f12ada0c516535899421095539807c1d16e044
ajitrai1507 <ajitrai0715@gmail.com>
2024-12-23 20:54:27 +0530

    Conditional Statement and loops

---
+++ 18/script.js
@@ -0,0 +1,111 @@
+//If-else statement
+// let age = 13;
+
+// if(age>=18){
+//     console.log("Eligible for vote");
+// }else{
+//     console.log("Not Eligible for vote");
+// }
+
+// let age = 20;
+
+// if(age<=18){
+//     console.log("You're Bacha");
+// }else if(age>45){
+//     console.log("You're old");
+// }else{
+//     console.log("You're young")
+// }
+
+// let day = 8;
+
+// switch(day){
+//     case 0:
+//         console.log("Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Not a valid date");
+// }
+// switch(new Date().getDay()){
+//     case 0:
+//         console.log("Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Not a valid date");
+// }
+
+
+//For Loop 
+
+// for(let i=1; i<=5; i++){
+//     console.log("hello coder army");
+// }
+
+//Sum of first 10 numbers
+
+// let sum = 0;
+
+// for(let i=1; i<=10; i++){
+
+//     sum = sum+i;
+// }
+
+// console.log(sum);
+
+
+//Nested for loop
+
+// for(let i=1; i<=6; i++){
+
+//     for(let j=1; j<=5; j++){
+//         console.log(i);
+//     }
+   
+// }
+// let i=1;
+// while(i<=5){
+//     console.log("Hello jii")
+//     i++;
+// }
+
+// let arr = [5,10,15,20,25];
+
+// for(let i=0; i<=arr.length; i++){
+//     console.log(arr[i]);
+// }
